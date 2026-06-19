dffe9cc4f04061485605fe14074ba89780f19207
elizabethmacharia366-art <elizabethmacharia366@gmail.com>
2026-06-19 11:05:53 +0300

    fix:NVENTORY & STOCK CONTROL

--- api/src/stock/stock.controller.ts
+++ api/src/stock/stock.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, ValidationPipe } from '@nestjs/common';
 import { StockService } from './stock.service';
 
 @Controller('stock')
@@ -6,8 +6,27 @@ export class StockController {
   constructor(private readonly stockService: StockService) {}
 
   @Post('purchase')
-  async purchase(@Body() body: any) {
-    return this.stockService.createPurchase();
+  async purchase(@Body(ValidationPipe) body: any) {
+    return this.stockService.createPurchase(body);
+  }
+
+  @Post('adjustment')
+  async adjustment(@Body(ValidationPipe) body: any) {
+    return this.stockService.createAdjustment(body);
+  }
+
+  @Post('transfer')
+  async transfer(@Body(ValidationPipe) body: any) {
+    return this.stockService.createTransfer(body);
+  }
+
+  @Get('product/:productId')
+  async getStockItem(@Param('productId') productId: string) {
+    return this.stockService.getStockItem(parseInt(productId));
+  }
+
+  @Get()
+  async getAllStockItems() {
+    return this.stockService.getAllStockItems();
   }
 }
-
