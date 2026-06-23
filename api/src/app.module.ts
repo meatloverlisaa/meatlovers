0a849463e4b75ed1f44b0614a28efa0657652f9b
elizabethmacharia366-art <elizabethmacharia366@gmail.com>
2026-06-23 10:59:56 +0300

    feat(api): register waste module in app module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -14,6 +14,7 @@ import { BarModule } from './bar/bar.module';
 import { RecipesModule } from './recipes/recipes.module';
 import { ProductionPlansModule } from './production-plans/production-plans.module';
 import { DeliveriesModule } from './deliveries/deliveries.module';
+import { WasteModule } from './waste/waste.module';
 
 
 @Module({
@@ -31,6 +32,7 @@ import { DeliveriesModule } from './deliveries/deliveries.module';
     RecipesModule,
     ProductionPlansModule,
     DeliveriesModule,
+    WasteModule,
   ],
   controllers: [AppController],
   providers: [AppService],
