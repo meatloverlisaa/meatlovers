7ad08681954636b22d95bd9778bfc4d109ea1e41
elizabethmacharia366-art <elizabethmacharia366@gmail.com>
2026-08-19 15:26:27 +0300

    fix:visibility on  login

---
+++ ui/src/types/heroicons.d.ts
@@ -0,0 +1,36 @@
+declare module '@heroicons/react/24/outline' {
+  import { SVGProps } from 'react';
+  export const ChartBarIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ClipboardDocumentListIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const GlobeAltIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CubeIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CurrencyDollarIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const BuildingOfficeIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const BeakerIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const WrenchIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CreditCardIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const TruckIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const TrashIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const UserGroupIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const UserIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const BanknotesIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CheckCircleIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ShieldCheckIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CogIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ExclamationTriangleIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ArrowRightOnRectangleIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ShieldExclamationIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ArrowPathIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const DocumentTextIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const XCircleIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ScaleIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ChartPieIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ClockIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const ShoppingCartIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const CalendarIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const WrenchScrewdriverIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const PlusIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const MapPinIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const EyeIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+  export const EyeSlashIcon: (props: SVGProps<SVGSVGElement>) => JSX.Element;
+}
